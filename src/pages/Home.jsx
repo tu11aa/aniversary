b3cecdb88842980a5c73e876c6bc1131b9d5ac75
tu11aa <thanhtuhr01@gmail.com>
2022-09-16 21:30:36 +0700

    add giftbox

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import "../css/giftbox.css"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className="giftbox" onClick={()=>console.log("Clicked")}>
+        <div className="cover">
+            <div></div>
+        </div>
+        
+        <div className="box"></div>
+      </div>
+    </>
   )
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import "../login.scss"
+import "../css/login.scss"
 import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
